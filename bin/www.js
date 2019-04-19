ad4a2bc5ff31ece6a6f0e619ed7243864eb5e594
Kevin Isom <kevin@isom.nz>
2019-04-20 11:56:29 +1200

    Init

--- bin/www.js
+++ bin/www.js
@@ -2,7 +2,7 @@
 var debug = require('debug')('my-application');
 var app = require('../app');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
---
+++ src/webpack/dev.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const baseConfig = require('./config')
+
+const devConfig = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      template: './src/ui/index.html',
+      historyApiFallback: true,
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: false,
+    proxy: {
+      '/graphql': 'http://localhost:4567/',
+    },
+  },
+}
+
+module.exports = {
+  ...baseConfig,
+  ...devConfig,
+}
