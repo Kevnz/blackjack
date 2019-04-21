25df9e5f3d842ecdbb6acfb44a72e7c0e2d4ffaf
Kevin Isom <kevin@isom.nz>
2019-04-21 15:22:45 +1200

    Add routes and start at home

--- src/ui/core/app.js
+++ src/ui/core/app.js
@@ -2,17 +2,21 @@ import React, { Component, Fragment } from 'react'
 import { Router } from '@reach/router'
 
 const About = React.lazy(() => import('../features/about'))
-const Home = React.lazy(() => import('../games/blackjack'))
+const Home = React.lazy(() => import('../features/home'))
+const Blackjack = React.lazy(() => import('../games/blackjack'))
+const Dice = React.lazy(() => import('../games/dice'))
 const Contact = React.lazy(() => import('../features/contact'))
 
 export default class App extends Component {
   render() {
     return (
       <Fragment>
-        <h1>App</h1>
+        <h1>Games</h1>
         <React.Suspense fallback={<div>Loading</div>}>
           <Router>
             <Home path="/" />
+            <Blackjack path="/blackjack" />
+            <Dice path="/dice" />
             <About path="/about" />
             <Contact path="/contact" />
           </Router>
