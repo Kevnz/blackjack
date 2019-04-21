5fde045e1522b89a8756e48b6cfc903f4c8fded2
Kevin Isom <kevin@isom.nz>
2019-04-21 14:54:27 +1200

    pull in build deck from card engine

--- src/ui/games/blackjack.js
+++ src/ui/games/blackjack.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Button } from 'react-form-elements'
+import { buildDeck } from '../engines/cards'
 import {
-  buildDeck,
   dealTheCardsOut,
   handleHit,
   playerStand,
