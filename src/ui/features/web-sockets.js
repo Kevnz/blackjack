bdca55022083f83f9d4ffa90559c2b032598fbe0
Kevin Isom <kevin.isom@gmail.com>
2019-08-01 22:28:39 +1200

    base setup

--- src/ui/features/web-sockets.js
+++ src/ui/features/web-sockets.js
@@ -1,7 +1,18 @@
 import React from 'react'
-import useNes from '../hooks/use-nes'
-export default () => {
-  const client = useNes()
+import { Container } from '@brightleaf/elements'
+import { useNes } from '@brightleaf/react-hooks/lib/use-nes'
 
-  return <div>WebSockets</div>
+export default () => {
+  const { message, error, connecting, connected } = useNes(
+    'wss://kev-pi.herokuapp.com'
+  )
+  // client.subscribe('/games/general', handler);
+  console.log('message', message)
+  return (
+    <Container>
+      {error && <div>Error</div>}
+      {connecting && <div>connecting</div>}
+      {connected && <div>connected</div>}
+    </Container>
+  )
 }
