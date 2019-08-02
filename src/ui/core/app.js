353e30b6b204d15f702dcc9da05bf8c39e6be4d7
Kevin Isom <kevin.isom@gmail.com>
2019-08-02 12:05:28 +1200

    Links

--- src/ui/core/app.js
+++ src/ui/core/app.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { Router } from '@reach/router'
+import { Router, Location, LocationProvider, Link } from '@reach/router'
 import {
   Button,
   Buttons,
@@ -20,66 +20,90 @@ const Blackjack = React.lazy(() => import('../games/blackjack'))
 const Dice = React.lazy(() => import('../games/dice'))
 const Contact = React.lazy(() => import('../features/contact'))
 
+const NavBarLink = ({ children, ...props }) => {
+  return (
+    <Location>
+      {({ location }) => {
+        const active = props.to === location.pathname
+        return (
+          <Link
+            className={active ? 'navbar-item is-active' : 'navbar-item '}
+            {...props}
+            getProps={prop => {
+              const { isCurrent } = prop
+              return {
+                className: isCurrent ? 'navbar-item is-active' : 'navbar-item ',
+              }
+            }}
+          >
+            {children}
+          </Link>
+        )
+      }}
+    </Location>
+  )
+}
+
 export default class App extends Component {
   render() {
     return (
-      <Fragment>
-        <NavBar isLight>
-          <NavBarBrand
-            src="games.png"
-            href="https://brightleaf.dev"
-            target="navbarBasicExample"
-            width="112"
-            height="68"
-          />
-          <NavBarMenu id="navbarBasicExample">
-            <NavBarStart>
-              <NavBarItem>
-                <a>Home</a>
-              </NavBarItem>
-
-              <NavBarDropDown title="Games">
+      <LocationProvider>
+        <Fragment>
+          <NavBar isLight>
+            <NavBarBrand
+              src="games.png"
+              href="https://brightleaf.dev"
+              target="navbarBasicExample"
+              width="112"
+              height="68"
+            />
+            <NavBarMenu id="navbarBasicExample">
+              <NavBarStart>
                 <NavBarItem>
-                  <a to="/containers">Blackjack</a>
-                </NavBarItem>
-                <NavBarItem>
-                  <a to="/columns">Dice</a>
+                  <Link to="/">Home</Link>
                 </NavBarItem>
 
-                <NavBarDivider />
+                <NavBarDropDown title="Games">
+                  <NavBarItem>
+                    <NavBarLink to="/blackjack">Blackjack</NavBarLink>
+                  </NavBarItem>
+                  <NavBarLink to="/dice">Dice</NavBarLink>
+
+                  <NavBarDivider />
+                  <NavBarItem>
+                    <a>Report an issue</a>
+                  </NavBarItem>
+                </NavBarDropDown>
+              </NavBarStart>
+
+              <NavBarEnd>
                 <NavBarItem>
-                  <a>Report an issue</a>
+                  <div>
+                    <Buttons>
+                      <Button isPrimary isAnchor>
+                        <strong>Sign up</strong>
+                      </Button>
+                      <Button isLight isAnchor>
+                        Log in
+                      </Button>
+                    </Buttons>
+                  </div>
                 </NavBarItem>
-              </NavBarDropDown>
-            </NavBarStart>
+              </NavBarEnd>
+            </NavBarMenu>
+          </NavBar>
 
-            <NavBarEnd>
-              <NavBarItem>
-                <div>
-                  <Buttons>
-                    <Button isPrimary isAnchor>
-                      <strong>Sign up</strong>
-                    </Button>
-                    <Button isLight isAnchor>
-                      Log in
-                    </Button>
-                  </Buttons>
-                </div>
-              </NavBarItem>
-            </NavBarEnd>
-          </NavBarMenu>
-        </NavBar>
-
-        <React.Suspense fallback={<div>Loading</div>}>
-          <Router>
-            <Home path="/" />
-            <Blackjack path="/blackjack" />
-            <Dice path="/dice" />
-            <About path="/about" />
-            <Contact path="/contact" />
-          </Router>
-        </React.Suspense>
-      </Fragment>
+          <React.Suspense fallback={<div>Loading</div>}>
+            <Router>
+              <Home path="/" />
+              <Blackjack path="/blackjack" />
+              <Dice path="/dice" />
+              <About path="/about" />
+              <Contact path="/contact" />
+            </Router>
+          </React.Suspense>
+        </Fragment>
+      </LocationProvider>
     )
   }
 }
